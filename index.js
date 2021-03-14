ea05302c285b0a41905279938b9845b2ec9b3c8f
Elijah Schiltz-Rouse <elijahforest101@gmail.com>
2021-03-14 19:35:29 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string){
+return string.toUpperCase();
+}
+
+function whisper(string) {
+return string.toLowerCase();
+}
+
+function logShout(string) {
+console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+
+upper = string.toUpperCase();
+lower = string.toLowerCase();
+
+if (string === lower) {
+returns "I can't hear you!";
+}
+
+if (string === upper) {
+returns "YES INDEED!";
+}
+
+if ( string === "I love you, Grandma."){
+returns "I love you, too.";
+}
+}
